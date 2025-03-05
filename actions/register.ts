da28ec5ddbf363422abf7fe89ea5efb73b817c28
luzumsuzadam <cengizulutas123123@gmail.com>
2025-03-05 22:09:51 +0300

    connected db, installed authjs

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,38 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export async function registerAction(values: z.infer<typeof registerSchema>) {
   const validatedFields = registerSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: "Invalid fields!"};
+    return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email.
+
+  return { success: "User created!" };
 }
